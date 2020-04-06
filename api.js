76cb138ed8a761d645ebd4f1bb8190b8b5e7e93c
joeprabawa <joeprabawa@gmail.com>
2020-04-07 07:42:03 +0800

    commit

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const SpotifyWebApi = require("spotify-web-api-node");
-const env = require("./env");
+const env = require("./env.js");
 
 const spotifyApi = new SpotifyWebApi({
   redirectUri: env.REDIRECT_URI,
