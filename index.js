bfda90eee3c8703ef65c72710a06bb316a14fa5d
joeprabawa <joeprabawa@gmail.com>
2024-01-23 20:48:37 +0800

    -  change start server log

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 app.listen({ port: process.env.PORT || 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`🚀 Server ready at http://localhost:4000/`)
 );
